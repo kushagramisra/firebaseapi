87836f70c396cb856476352e1df167a9b8507090
MKushagra <kushagramisra@yahoo.com>
2017-04-18 23:23:26 +0530

    Adding custom exception

---
+++ src/com/google/exceptions/IllegalDeviceId.java
@@ -0,0 +1,12 @@
+package com.google.exceptions;
+
+/**
+ * Created by MKushagra on 4/18/2017.
+ */
+public class IllegalDeviceId extends Exception {
+
+    public IllegalDeviceId(String exceptionString)
+    {
+        super(exceptionString);
+    }
+}
